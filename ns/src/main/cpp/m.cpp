08b0c7143544310f98151b1453b5324dc898d506
girllee <hodoinfo@gmail.com>
2021-03-23 01:40:04 +0800

    clean

--- ns/src/main/cpp/m.cpp
+++ ns/src/main/cpp/m.cpp
@@ -43,3 +43,34 @@ char * Jstring2CStr( JNIEnv * env, jstring jstr )
 
     return rtn;
 }
+
+extern "C"
+JNIEXPORT jintArray JNICALL
+Java_com_bcoin_ns_DataProvider_intMethod(JNIEnv *env, jobject thiz, jintArray i_num) {
+    // TODO: implement intMethod()
+}
+extern "C"
+JNIEXPORT jbyteArray JNICALL
+Java_com_bcoin_ns_DataProvider_byteMethod(JNIEnv *env, jobject thiz, jbyteArray i_byte) {
+    // TODO: implement byteMethod()
+}extern "C"
+JNIEXPORT jobject JNICALL
+Java_com_bcoin_ns_DataProvider_getStruct(JNIEnv *env, jobject thiz) {
+    // TODO: implement getStruct()
+}extern "C"
+JNIEXPORT jint JNICALL
+Java_com_bcoin_ns_DataProvider_add__II(JNIEnv *env, jobject thiz, jint x, jint y) {
+    // TODO: implement add()
+}extern "C"
+JNIEXPORT jint JNICALL
+Java_com_bcoin_ns_DataProvider_sub(JNIEnv *env, jclass clazz, jint x, jint y) {
+    // TODO: implement sub()
+}extern "C"
+JNIEXPORT jchar JNICALL
+Java_com_bcoin_ns_DataProvider_add__CC(JNIEnv *env, jobject thiz, jchar x, jchar y) {
+    // TODO: implement add()
+}extern "C"
+JNIEXPORT jstring JNICALL
+Java_com_bcoin_ns_DataProvider_sayHelloInC(JNIEnv *env, jobject thiz, jstring s) {
+    // TODO: implement sayHelloInC()
+}
--- ns/src/main/cpp/ns.cpp
+++ ns/src/main/cpp/ns.cpp
@@ -24,32 +24,4 @@ Java_com_bcoin_ns_S_decrypt(JNIEnv *env, jobject thiz, jstring s) {
     sign_str = sign_str.substr(0,sign_str.length()-2);
     env->ReleaseStringUTFChars(s,params);
     return env->NewStringUTF(sign_str.c_str());
-}extern "C"
-JNIEXPORT jbyteArray JNICALL
-Java_com_bcoin_ns_DataProvider_byteMethod(JNIEnv *env, jobject thiz, jbyteArray i_byte) {
-    // TODO: implement byteMethod()
-}extern "C"
-JNIEXPORT jobject JNICALL
-Java_com_bcoin_ns_DataProvider_getStruct(JNIEnv *env, jobject thiz) {
-    // TODO: implement getStruct()
-}extern "C"
-JNIEXPORT jint JNICALL
-Java_com_bcoin_ns_DataProvider_add__II(JNIEnv *env, jobject thiz, jint x, jint y) {
-    // TODO: implement add()
-}extern "C"
-JNIEXPORT jint JNICALL
-Java_com_bcoin_ns_DataProvider_sub(JNIEnv *env, jclass clazz, jint x, jint y) {
-    // TODO: implement sub()
-}extern "C"
-JNIEXPORT jchar JNICALL
-Java_com_bcoin_ns_DataProvider_add__CC(JNIEnv *env, jobject thiz, jchar x, jchar y) {
-    // TODO: implement add()
-}extern "C"
-JNIEXPORT jstring JNICALL
-Java_com_bcoin_ns_DataProvider_sayHelloInC(JNIEnv *env, jobject thiz, jstring s) {
-    // TODO: implement sayHelloInC()
-}extern "C"
-JNIEXPORT jintArray JNICALL
-Java_com_bcoin_ns_DataProvider_intMethod(JNIEnv *env, jobject thiz, jintArray i_num) {
-    // TODO: implement intMethod()
 }
